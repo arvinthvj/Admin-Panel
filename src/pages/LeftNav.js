7630901a472b863f69929d77edaa1d21e467842e
arvinthinnive <amanivannaboopathy@inniveinc.com>
2021-12-04 00:51:44 +0530

    nav left added and icons added

--- src/pages/LeftNav.js
+++ src/pages/LeftNav.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import "../Styles/leftNav.scss"
+import "../Styles/leftNav.scss";
+import {useState, useEffect} from 'react';
+import navData from './Data/LeftSideNavData';
 function LeftNav(props) {
+    const [SideNavNames, setSideNavNames] = useState(navData);
+const handleSideNavClick=(e)=>{
+    let navNames =  SideNavNames.map(e=>{return e});
+    navNames.forEach(o=>{
+        if(e.name == o.name){
+            o.selected = true
+        }else{
+            o.selected= false
+        }
+    })
+    
+    setSideNavNames(navNames)
+}
     return (
         <div className="LeftNav_totalContainer">
             <div className="LeftNav_subTotalContainer">
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
+            {SideNavNames.map((e,i)=>(
+                <>
+                <div onClick={()=>{handleSideNavClick(e)}} key={i} className={`LeftNav_names ${i} ${e.selected ? "selected": "notSelected"}`}>{e.name}</div>
+                </>
+            ))}
             </div>
         </div>
     );
