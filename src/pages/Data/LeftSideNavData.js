0d18d86657ddeca2ed52a2800c2a00a550cc3e36
arvinthinnive <amanivannaboopathy@inniveinc.com>
2021-12-04 00:52:53 +0530

    images and nav left added

---
+++ src/pages/Data/LeftSideNavData.js
@@ -0,0 +1,54 @@
+const navData=[
+        {
+            "name": "Dashboard",
+            "selected": true
+        },
+        {
+            "name": "Login",
+            "selected": false
+        },
+        {
+            "name": "Tables",
+            "selected": false
+        },
+        {
+            "name": "Pensing Payments",
+            "selected": false
+        },
+        {
+            "name": "Sign In",
+            "selected": false
+        },
+        {
+            "name": "Sign up",
+            "selected": false
+        },
+        {
+            "name": "Portals",
+            "selected": false
+        },
+        {
+            "name": "Cluster",
+            "selected": false
+        },
+        {
+            "name": "Time Taken",
+            "selected": false
+        },
+        {
+            "name": "Pensing Payments -Admin view",
+            "selected": false
+        },
+        {
+            "name": "Admin - View",
+            "selected": false
+        },
+        {
+            "name": "Corporate",
+            "selected": false
+        }
+    ]
+
+
+
+    export default navData;
