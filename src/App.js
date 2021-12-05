622be6e489957490f0b74a4be0d40d659934e722
arvinthinnive <amanivannaboopathy@inniveinc.com>
2021-12-05 21:07:44 +0530

    router added and signup page created

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from 'redux'
 import { actionCreators } from "./state/index"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SignUp from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='/dashboard'>
+            <Home />
+          </Route>
+          <Route path='/register'>
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
